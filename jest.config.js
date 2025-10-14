a24af0bd176b9ff5800ae2c9fb672f6699bd4136
Stefanie Jane <stef@hyperbliss.tech>
2025-10-14 09:02:18 -0700

    test: 🧪 set up test infrastructure for Next.js 15
    Create centralized test setup to mock Next.js server APIs:
    
    - Mock `next/headers` (headers, cookies) for testing
    - Mock `next/server` (connection) for async component tests
    - Add global `mockWindow()` and `unmockWindow()` helpers for
      simulating browser environment with window.__ENV
    - Configure Jest to use setupFilesAfterEnv
    - Convert jest.config to ES module format
    
    This infrastructure allows tests to run without "outside request
    scope" errors and properly simulate both server and browser
    environments.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
+export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   testPathIgnorePatterns: [
     '<rootDir>/node_modules/',
     '<rootDir>/*.js',
@@ -15,5 +16,6 @@ module.exports = {
     '<rootDir>/build',
     '<rootDir>/examples',
     '<rootDir>/src/lib',
+    '<rootDir>/src/test-setup.ts',
   ],
 };
